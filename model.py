f01ded85c6c5e86a7fcd1cc1fa8c48ce9fea7be3
matherskishore <kishore.r@integrumsolutions.in>
2023-02-03 15:31:40 +0530

    second commit

--- model.py
+++ model.py
@@ -7,17 +7,19 @@ app = Flask(__name__)
 
 @app.route('/parse-cv', methods=['GET'])
 def parse():
-    filename = request.args.get('filename')
+    for item in input(f"upload your CVs here please: "):
+        filename = request.args.get(item)
 
-    if filename is None:
-        text = "please upload at least one file to be parsed"
+        if filename is None:
+            text = "please upload at least one file to be parsed"
 
-    else:
-        cv_data = ResumeParser(filename) \
-            .get_extracted_data()
-        text = json.dumps(cv_data)
+        else:
+            cv_data = ResumeParser(filename)\
+                .get_extracted_data()
+            text = json.dumps(cv_data)
 
-    return text
+        return text
+
 
 parse()
 
